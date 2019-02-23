6181934922dba7e7694866ca2c2b842db3c22dc4
piedcipher <pateltirth454@gmail.com>
2019-02-24 02:37:44 +0530

    Completed First Project

--- #1.Business-Card/business_card/lib/main.dart
+++ #1.Business-Card/business_card/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final String _busniessUrl = "http://www.piedpiper.com";
+  final String _businessLocation = "https://goo.gl/maps/Lnh3S5x82VG2";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,102 +18,81 @@ class MyApp extends StatelessWidget {
           child: ListView(
             children: <Widget>[
               Image.asset("assets/piedpiper_header.png"),
-              Center(
-                child: Container(
-                  margin: EdgeInsets.all(18),
-                  child: Text(
-                    "Go Small or Go Home",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold),
-                  ),
+              aboutBusiness(
+                  text: "Go Small or Go Home",
+                  textSize: 24,
+                  fontWeight: FontWeight.bold),
+              aboutBusiness(
+                  text:
+                      "A Middle-out Compression Solution to make Data Storage Problems smaller. We take compression to a new level with Weissman Score of 5.2",
+                  textSize: 20,
+                  fontStyle: FontStyle.italic),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 4),
+                child: Divider(
+                  color: Colors.white,
                 ),
               ),
-              Center(
-                child: Container(
-                  margin: EdgeInsets.all(16),
-                  child: Text(
-                    "A Middle-out Compression Solution to make Data Storage Problems smaller. We take compression to a new level with Weissman Score of 5.2",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontStyle: FontStyle.italic,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 4,
-                  ),
-                  child: Container(
-                    height: 1,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 28),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Icon(
-                          Icons.public,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: Text(
-                          "piedpiper.com",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 19,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Icon(
-                          Icons.location_on,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 4,
-                        child: Text(
-                          "5230 Penfield Ave, Woodland Hills, CA",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 19,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
+              contactBusiness(
+                  text: "piedpiper.com", url: _busniessUrl, icon: Icons.public),
+              contactBusiness(
+                  text: "5230 Penfield Ave, Woodland Hills, CA",
+                  url: _businessLocation,
+                  icon: Icons.location_on),
             ],
           ),
         ),
       ),
     );
+  }
+
+  Widget aboutBusiness(
+      {@required String text,
+      @required double textSize,
+      FontWeight fontWeight,
+      FontStyle fontStyle}) {
+    return Container(
+      margin: EdgeInsets.all(16),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: textSize,
+          fontWeight: fontWeight,
+          fontStyle: fontStyle,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Widget contactBusiness(
+      {@required String text, @required String url, @required IconData icon}) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          color: Colors.white,
+        ),
+        onTap: () => launchUrl(url),
+        title: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 19,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Can\'t launch $url';
+    }
   }
 }
